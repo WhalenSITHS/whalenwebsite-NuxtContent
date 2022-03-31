4cb83c4e4eacf2537337bd5342dd214c37e71d84
mr. whalen <michael.whalen@sitechhs.com>
2022-03-31 12:26:02 -0400

    d

--- site/nuxt.config.js
+++ site/nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'site',
+    title: 'whalen',
     htmlAttrs: {
       lang: 'en',
     },
@@ -18,7 +18,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['~assets/main.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
