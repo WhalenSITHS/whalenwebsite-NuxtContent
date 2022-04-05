c001ae4b3d65dd9631f1888098c737c0b3c53754
mr. whalen <michael.whalen@sitechhs.com>
2022-04-05 14:50:29 -0400

    need to add in filters for resources

---
+++ site/components/Resource.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="resource-card">
+    <img :src="getImage" :alt="altText" class="resource-img" />
+    <h2 class="resource-title">{{ title }}</h2>
+    <p class="resource-text">
+      {{ text }}
+    </p>
+    <a class="resource-btn" :href="link" target="_blank">Visit</a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    text: String,
+    image: String,
+    alt: String,
+    link: String,
+  },
+  computed: {
+    getImage: function () {
+      return require(`~/assets/public/${this.image}.png`)
+    },
+    altText: function () {
+      return this.alt
+    },
+    getLink: function () {
+      return this.link
+    },
+  },
+}
+</script>
+
+<style>
+.resource-card {
+  width: 21%;
+  display: flex;
+  height: 40vh;
+  padding: 1rem 2rem;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  background-color: #fff;
+  color: var(--primary);
+  border-radius: 10px;
+  margin: 3rem auto;
+}
+.resource-img {
+  margin-top: 1rem;
+  width: 100%;
+  height: 50%;
+  object-fit: cover;
+}
+.resource-title {
+  font-size: var(--h4);
+  margin: 1rem 0;
+  text-align: left;
+}
+.resource-text {
+  font-size: var(--h5);
+  margin: 1rem 0;
+  text-align: center;
+}
+.resource-btn {
+  font-size: var(--h4);
+  width: 15rem;
+  height: 3.5rem;
+  color: var(--white);
+  border: 2px solid transparent;
+  padding: 0.3rem 2rem;
+  text-decoration: none;
+  transition: all 0.4s;
+  background-color: transparent;
+  border-radius: 3rem;
+  transition: all 0.3s;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  margin: 1rem auto;
+  background-color: var(--secondary);
+}
+
+.resource-btn:hover {
+  transform: translateY(-2px);
+  box-shadow: 0 10px 0.5rem rgba(color(black), 0.6);
+}
+.resource-btn::after {
+  content: '';
+  display: inline-block;
+  height: 100%;
+  width: 100%;
+  border-radius: 3rem;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+  transition: all 0.3s;
+}
+.resource-btn:hover::after {
+  transform: scaleX(1.5) scaleY(1.6);
+  opacity: 0;
+}
+.resource-btn:active {
+  transform: translateY(-1px);
+  transform: scaleX(1.5) scaleY(1.6);
+  box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.2);
+}
+</style>
---
+++ site/pages/Resources.vue
@@ -0,0 +1,50 @@
+<template>
+  <section class="resources">
+    <h2 class="section-title">Development</h2>
+    <div class="resource-section">
+      <Resource
+        v-for="resource in resources.resources"
+        :key="resource.name"
+        :title="resource.name"
+        :image="resource.thumbnail"
+        :link="resource.link"
+        :alt="resource.alt"
+        :text="resource.description"
+      ></Resource>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      resources: null,
+    }
+  },
+  async fetch() {
+    this.resources = await this.$content('resources').fetch()
+  },
+}
+</script>
+
+<style>
+.resources {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  margin: 5rem auto;
+}
+.section-title {
+  font-size: var(--h1);
+}
+.resource-section {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  width: 80vw;
+  margin: 5rem auto;
+}
+</style>
