37b83299c4cd5d1cf7f81d58f7d6539a3989dcb2
mr. whalen <michael.whalen@sitechhs.com>
2022-04-14 08:45:43 -0400

    kujhgvk

---
+++ site/components/GitLink.vue
@@ -0,0 +1,22 @@
+<template>
+  <a class="git-link" :href="getLink" target="_blank">Github</a>
+</template>
+
+<script>
+export default {
+  props: {
+    sourceLink: String,
+  },
+  computed: {
+    getLink: function () {
+      return this.sourceLink
+    },
+  },
+}
+</script>
+
+<style>
+.git-link {
+  font-size: var(--h4);
+}
+</style>
