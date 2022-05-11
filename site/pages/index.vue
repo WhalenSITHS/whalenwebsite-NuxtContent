10b399da2224e72dd67bc2261a4a5fdb93ecba84
mr. whalen <michael.whalen@sitechhs.com>
2022-05-11 14:12:07 -0400

    cool

--- site/pages/index.vue
+++ site/pages/index.vue
@@ -26,6 +26,27 @@ export default {
       })
     }, */
     animateOnScroll() {
+      const tlH = this.$gsap.timeline({
+        delay: 0.1,
+      })
+
+      tlH.from('.header-animation', {
+        duration: 0.3,
+        opacity: 0,
+        stagger: 0.2,
+        x: -350,
+      })
+      tlH.from('.header-image', { duration: 0.5, y: 600, rotate: 90 }, '<.2')
+      tlH.from(
+        '.header-animation-2',
+        {
+          duration: 0.3,
+          opacity: 0,
+          stagger: 0.2,
+        },
+        '<.2'
+      )
+
       const tl = this.$gsap.timeline({
         scrollTrigger: '.about',
         delay: 0.1,
@@ -41,6 +62,7 @@ export default {
         '.about-img',
         {
           opacity: 0,
+          delay: 0.1,
           duration: 0.5,
           ease: 'Power1.easeInOut',
         },
@@ -52,6 +74,7 @@ export default {
         {
           opacity: 0,
           duration: 0.5,
+          delay: 0.2,
           x: 150,
           //stagger: 0.1,
           ease: 'Power1.easeInOut',
@@ -62,7 +85,7 @@ export default {
         '.about-btn-animation',
         {
           opacity: 0,
-
+          delay: 0.3,
           duration: 0.3,
         },
         '<'
