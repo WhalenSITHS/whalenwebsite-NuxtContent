81561b570666f008c193f901a0ee3fbe6db7de27
mr. whalen <michael.whalen@sitechhs.com>
2022-05-18 13:09:16 -0400

    diex access footers

--- site/components/Footer.vue
+++ site/components/Footer.vue
@@ -7,32 +7,48 @@
         rel="noopener noreferrer"
       >
         <span class="icon footer-link"
-          ><fa class="footer-icon" icon="envelope-square" /></span
-      ></a>
+          ><fa class="footer-icon" icon="envelope-square" />
+          <span class="visually-hidden"
+            >E-mail Me (opens in new tab)></span
+          ></span
+        ></a
+      >
       <a
         href="https://www.facebook.com/michael.whalen.180"
         target="_blank"
         rel="noopener noreferrer"
         class="footer-link"
         ><span class="icon footer-link"
-          ><fa class="footer-icon" :icon="['fab', 'facebook-square']" /></span
-      ></a>
+          ><fa class="footer-icon" :icon="['fab', 'facebook-square']" />
+          <span class="visually-hidden"
+            >Facebook Link(opens in new tab)></span
+          ></span
+        ></a
+      >
       <a
         href="https://twitter.com/whalensiths"
         target="_blank"
         rel="noopener noreferrer"
         class="footer-link"
         ><span class="icon footer-link"
-          ><fa class="footer-icon" :icon="['fab', 'twitter-square']" /></span
-      ></a>
+          ><fa class="footer-icon" :icon="['fab', 'twitter-square']" />
+          <span class="visually-hidden"
+            >Tweet Me (opens in new tab)></span
+          ></span
+        ></a
+      >
       <a
         href="https://github.com/WhalenSITHS"
         target="_blank"
         rel="noopener noreferrer"
         class="footer-link"
         ><span class="icon footer-link"
-          ><fa class="footer-icon" :icon="['fab', 'github-square']" /></span
-      ></a>
+          ><fa class="footer-icon" :icon="['fab', 'github-square']" />
+          <span class="visually-hidden"
+            >My Github (opens in new tab)></span
+          ></span
+        ></a
+      >
     </div>
   </footer>
 </template>
@@ -64,5 +80,27 @@ footer {
 }
 .footer-icon {
   font-size: var(--h1);
+}
+.visually-hidden {
+  border: 0;
+  padding: 0;
+  margin: 0;
+  position: absolute !important;
+  height: 1px;
+  width: 1px;
+  overflow: hidden;
+  clip: rect(
+    1px 1px 1px 1px
+  ); /* IE6, IE7 - a 0 height clip, off to the bottom right of the visible 1px box */
+  clip: rect(
+    1px,
+    1px,
+    1px,
+    1px
+  ); /*maybe deprecated but we need to support legacy browsers */
+  clip-path: inset(
+    50%
+  ); /*modern browsers, clip-path works inwards from each corner*/
+  white-space: nowrap; /* added line to stop words getting smushed together (as they go onto seperate lines and some screen readers do not understand line feeds as a space */
 }
 </style>
